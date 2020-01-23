d0266e00319a8ee9dc6cf4af424621156bb9212c
Michael Chen <cwchen123@gmail.com>
2020-01-23 09:10:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Use C Code in Golang with cgo
+
+This repo demos how to use C code in Golang with [cgo](https://golang.org/cmd/cgo/).
+
+## Copyright
+
+Copyright (c) 2020 Michael Chen. Licensed under Apache 2.0
---
+++ pointDemo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"go-c-mix/point"
+	"log"
+)
+
+func main() {
+	p := point.NewPoint(0.0, 0.0)
+	q := point.NewPoint(3.0, 4.0)
+
+	dist := point.Distance(p, q)
+	if 5 != dist {
+		log.Fatal("Wrong distance")
+	}
+
+	p.Delete()
+	q.Delete()
+}
